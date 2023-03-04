c6493a3b29578f52b08e3582de20be90ea75dc73
joao-silva09 <joao.e.silva.03@gmail.com>
2023-03-04 19:58:54 -0300

    Create DataContext.

--- eWallet.Infrastructure/Data/DataContext.cs
+++ eWallet.Infrastructure/Data/DataContext.cs
@@ -1,22 +1,42 @@
 ﻿using eWallet.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace eWallet.Infrastructure.Data
 
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<ApplicationUser>
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DataContext()
         {
-
         }
 
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Conta> Contas { get; set; }
         public DbSet<Divida> Dividas { get; set; }
         public DbSet<Objetivo> Objetivos { get; set; }
         public DbSet<Operacao> Operacoes { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=JOAO\\SQLSERVER;Initial Catalog=eWalletAPI;Integrated Security=True; TrustServerCertificate=True;";
+        }
     }
 }
 
