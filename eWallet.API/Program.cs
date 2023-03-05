2f6a506e44deeb93ea12570dbcdd606f3aed8b8a
joao-silva09 <joao.e.silva.03@gmail.com>
2023-03-05 11:01:44 -0300

    Update Program.cs

--- eWallet.API/Program.cs
+++ eWallet.API/Program.cs
@@ -1,15 +1,68 @@
+using AutoMapper;
+using eWallet.API.Configurations;
+using eWallet.API.Configurations.Token;
+using eWallet.API.Services;
+using eWallet.API.Services.DividaService;
+using eWallet.API.Services.ObjetivoService;
+using eWallet.API.Services.OperacaoService;
 using eWallet.Infrastructure.Data;
+using eWallet.Services.IDividaService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DataContext>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//builder.Services.AddScoped<IContaService, ContaService>();
+//builder.Services.AddScoped<IDividaService, DividaService>();
+//builder.Services.AddScoped<IObjetivoService, ObjetivoService>();
+//builder.Services.AddScoped<IOperacaoService, OperacaoService>();
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "eWalletAPI",
+            ValidAudience = "eWalletAPI",
+            IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+        };
+
+        option.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                return Task.CompletedTask;
+            },
+            OnTokenValidated = context =>
+            {
+                Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 var app = builder.Build();
 
@@ -20,10 +73,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+var devClient = "http://localhost:4200";
+app.UseCors(x => x
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader().WithOrigins(devClient));
+
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSwaggerUI();
 
 app.Run();
